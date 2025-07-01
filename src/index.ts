2bc64a4e05f40761e9a97d8f77535f9cd727546c
Zach Philipp <zachphilipp@icloud.com>
2025-07-01 11:01:18 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export type { ActiveDirectoryConstructor } from './types/ad';
---
+++ src/types/ad.ts
@@ -0,0 +1,6 @@
+export interface ActiveDirectoryConstructor {
+  url: string;
+  baseDN: string;
+  username: string;
+  password: string;
+}
