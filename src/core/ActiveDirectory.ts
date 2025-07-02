7dbcc71c6077f5706ac18a7749ffac504107399f
Zach Philipp <zachphilipp@icloud.com>
2025-07-01 21:55:34 -0500

    init first class constructor

--- src/core/ActiveDirectory.ts
+++ src/core/ActiveDirectory.ts
@@ -1,3 +1,25 @@
-export class ActiveDirectory {}
+import z from 'zod';
 
-const _ = ';';
+import type { ActiveDirectoryConstructor } from '../types/ad';
+
+const ActiveDirectoryContructorSchema = z.object({
+  url: z.string(),
+  baseDN: z.string(),
+  username: z.string(),
+  password: z.string(),
+});
+
+export class ActiveDirectory {
+  url: string; // The url of the ldap server
+  baseDN: string; // Base container where all LDAP queries originate from. (i.e. dc=domain,dc=com)
+  username: string; // Username the administrative username or dn of the user for retrieving user & group information.
+  password: string; // Password of the user
+
+  constructor(config: ActiveDirectoryConstructor) {
+    ActiveDirectoryContructorSchema.parse(config);
+    this.url = config.url;
+    this.baseDN = config.baseDN;
+    this.username = config.username;
+    this.password = config.password;
+  }
+}
